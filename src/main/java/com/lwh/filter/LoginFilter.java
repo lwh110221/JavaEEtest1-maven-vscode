2eae610a03a922b6a4ba43bd8349c8ab33f0c2be
lwh110221 <2285813721@qq.com>
2024-04-23 08:23:41 +0800

    @LWH110221 注释

--- src/main/java/com/lwh/filter/LoginFilter.java
+++ src/main/java/com/lwh/filter/LoginFilter.java
@@ -13,23 +13,42 @@ import java.io.IOException;
  */
 @WebFilter("/*")
 public class LoginFilter implements Filter {
+
+    /**
+     * 登录过滤器
+     * @param filterConfig
+     * @throws ServletException
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+
+    /**
+     * 登录过滤器
+     * @param request
+     * @param response
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
         String uri = req.getRequestURI();
+        // 如果是登录页面或者登录请求，直接放行
         if (uri.contains("/login.jsp") || uri.contains("/login") || uri.contains("/resources/css/") || uri.contains("resources/fonts/") || uri.contains("resources/img/")) {
             chain.doFilter(request, response);
         } else {
+            // 如果是其他请求，判断是否登录
             Object student = req.getSession().getAttribute("student");
             if (student != null) {
+                // 如果已经登录，放行
                 chain.doFilter(request, response);
             } else {
+                // 如果没有登录，跳转到登录页面
                 res.sendRedirect("login.jsp");
             }
         }
